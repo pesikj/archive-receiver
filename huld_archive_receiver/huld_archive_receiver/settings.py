3da69bf10c7ba5d812a4297248dfae6d669bf042
Jiří Pešík <jiri.pesik@outlook.cz>
2023-08-17 20:00:29 +0200

    Updates

--- huld_archive_receiver/huld_archive_receiver/settings.py
+++ huld_archive_receiver/huld_archive_receiver/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "receiver.apps.ReceiverConfig"
+    "receiver.apps.ReceiverConfig",
+    'letsencrypt',
 ]
 
 MIDDLEWARE = [
--- huld_archive_receiver/huld_archive_receiver/urls.py
+++ huld_archive_receiver/huld_archive_receiver/urls.py
@@ -13,11 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import include, path
+from django.urls import include, path, re_path
 
 from huld_archive_receiver.settings import AUTH_URL_SECRET
 
 urlpatterns = [
     path("", include("receiver.urls")),
     path(f"{AUTH_URL_SECRET}/", include('django.contrib.auth.urls')),
+    re_path(r'^\.well-known/', include('letsencrypt.urls')),
 ]
